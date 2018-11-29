3e8ce21606b4134fd0fc20c2466cbb428ceb1abb
Gabriel Santiago <gisantiago@live.com>
2018-11-29 09:22:02 -0500

    Added functions to script.js

--- js/script.js
+++ js/script.js
@@ -1,44 +1,87 @@
 /******************************************
-Treehouse Techdegree:
-FSJS project 1 - A Random Quote Generator
+A Random Quote Generator
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
+
 
 
 /*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-
-  Recommended: 
-    - Add at least one `year` and/or `citation` property to at least one 
-      quote object.
+  Array of quote objects
 ***/
+const quotes = [
+  {
+    quote: "The people who are crazy enough to think they can change the world are the ones who do.",
+    source: "Steve Jobs",
+    year: 1997,
+    citation: "Apple's 1997 Think Different campaign"
+  },
+  {
+    quote: "You know you’re in love when you can’t fall asleep because reality is finally better than your dreams.",
+    source: "Dr. Suess",
+    year: 2013,
+    citation: "WikiQuote"
+  },
+  {
+    quote: "It Doesn’t Matter Where You Came From. All That Matters Is Where You Are Going.",
+    source: "Brian Tracy",
+    year: 2018
+  },
+  {
+    quote: "You can avoid reality, but you cannot avoid the consequences of avoiding reality.",
+    source: "Ayn Rand",
+    year: "1982"
+  },
+  {
+    quote: "The task of the modern educator is not to cut down jungles but to irrigate deserts.",
+    source: "CS Lewis",
+    year: "1943",
+    citation: "The Abolition of Man"
+  },
+  {
+    quote: "Love is not affectionate feeling, but a steady wish for the loved person's ultimate good as far as it can be obtained.",
+    source: "CS Lewis",
+    year: "1970",
+    citation: "God In The Dock"
+  },
+];
 
 
 
 
 /***
-  Create the `getRandomQuote` function to:
+  The `getRandomQuote` function will:
    - generate a random number 
-   - use the random number to `return` a random quote object from the 
-     `quotes` array.
 ***/
-
+function getRandomQuote(arr) {
+  quote = quotes[Math.floor( Math.random() * quotes.length)];
+  quote[arr];
+  return quote;
+}
 
 
 
 /***
-  Create the `printQuote` function to: 
-   - call the `getRandomQuote` function and assign it to a variable.
-   - use the properties of the quote object stored in the variable to 
-     create your HTML string.
-   - use conditionals to make sure the optional properties exist before 
-     they are added to the HTML string.
+  The `printQuote` function: 
+   - call the `getRandomQuote` function.
+   - using the properties of the quote objects assign HTML string to `quoteHTML`
+   - a pair of conditionals statements alow for the optional properties (citation and year) to be printed if they exist 
    - set the `innerHTML` of the `quote-box` div to the HTML string. 
 ***/
-
+function printQuote() {
+  var randomQuote = getRandomQuote();
+  var quoteHTML = `
+        <p class="quote">${randomQuote.quote}</p>
+        <p class="source">${randomQuote.source}`;
+        if (randomQuote.citation){
+          quoteHTML += `<span class="citation"> ${randomQuote.citation} </span>`;
+        }
+        if (randomQuote.year) {  
+          quoteHTML += `<span class="year"> ${randomQuote.year} </span>`;
+        }
+  quoteHTML += `</p>`;
+  document.getElementById('quote-box').innerHTML = quoteHTML;
+  return quoteHTML;
+}
 
 
 
@@ -51,5 +94,3 @@ FSJS project 1 - A Random Quote Generator
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
